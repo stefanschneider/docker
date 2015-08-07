949c7b5a5e5fbf0494ff7ae0d6439741cd459729
John Howard <jhoward@microsoft.com>
2015-08-06 19:21:00 -0700

    Windows: Enable NAT port mapping
    Signed-off-by: John Howard <jhoward@microsoft.com>
    
    Conflicts:
    	daemon/execdriver/windows/run.go

---
+++ daemon/execdriver/driver_windows.go
@@ -0,0 +1,23 @@
+package execdriver
+
+import "github.com/docker/docker/pkg/nat"
+
+// Network settings of the container
+type Network struct {
+	Interface      *NetworkInterface `json:"interface"`
+	Mtu            int               `json:"mtu"`
+	ContainerID    string            `json:"container_id"` // id of the container to join network.
+	NamespacePath  string            `json:"namespace_path"`
+	HostNetworking bool              `json:"host_networking"`
+}
+
+// NetworkInterface contains network configs for a driver
+type NetworkInterface struct {
+	MacAddress string `json:"mac"`
+	Bridge     string `json:"bridge"`
+	IPAddress  string `json:"ip"`
+
+	// PortBindings is the port mapping between the exposed port in the
+	// container and the port on the host.
+	PortBindings nat.PortMap `json:"port_bindings"`
+}
